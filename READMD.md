983061e53083e4fdf8ff9d095aeddf0e15ff878b
takzobye <takzobye@gmail.com>
2023-09-23 03:43:12 +0700

    Init

---
+++ READMD.md
@@ -0,0 +1,2 @@
+### This script was created from the forum link below.
+[CFX Forum : [How-To] Get the Chameleon paints](https://forum.cfx.re/t/how-to-get-the-chameleon-paints/4869883)
---
+++ fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version "cerulean"
+game "gta5"
+
+files { "carcols_gen9.meta", "carmodcols_gen9.meta" }
+data_file "CARCOLS_GEN9_FILE" "carcols_gen9.meta"
+data_file "CARMODCOLS_GEN9_FILE" "carmodcols_gen9.meta"
